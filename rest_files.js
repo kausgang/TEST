da511d211d895d5ed0e3e420482245960aae80d7
kausgang <write.to.kaustav@gmail.com>
2024-09-09 18:01:43 -0400

    Update rest_files.js

--- rest_files.js
+++ rest_files.js
@@ -2,8 +2,9 @@ const axios = require("axios");
 
 // Repository details and commit SHA
 const REPO_OWNER = "kausgang"; // Replace with your GitHub repository owner (username or organization)
-const REPO_NAME = "test"; // Replace with your GitHub repository name
-const COMMIT_SHA = "6ef2482f79976566ef99fccc8199ff77e2b869ae";
+const REPO_NAME = "TechLearning"; // Replace with your GitHub repository name
+const COMMIT_SHA = "888d9c143752f76eaab9ade834a0b7b4661c976f";
+GITHUB_TOKEN = "";
 
 // Function to get changed files in a commit
 async function getChangedFiles(commitSha) {
@@ -12,7 +13,7 @@ async function getChangedFiles(commitSha) {
       `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/commits/${commitSha}`,
       {
         headers: {
-          //   Authorization: `token ${GITHUB_TOKEN}`,
+          Authorization: `token ${GITHUB_TOKEN}`,
           Accept: "application/vnd.github.v3+json",
         },
       }
@@ -27,13 +28,6 @@ async function getChangedFiles(commitSha) {
     );
     throw error;
   }
-
-  //   try {
-  //     const response = axios({
-  //       url: "https://api.github.com/repos/kausgang/test/commits/846b934d5ecd8418ddcce150d6de37a047781460",
-  //       method: "GET",
-  //     });
-  //   } catch (error) {}
 }
 
 // Execute the function and print the result
