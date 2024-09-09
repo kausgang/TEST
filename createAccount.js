e34ba98e74065e3fe935caa49e959207d8830048
kausgang <write.to.kaustav@gmail.com>
2024-09-09 15:14:57 -0400

    Update createAccount.js

--- createAccount.js
+++ createAccount.js
@@ -2,38 +2,34 @@ const axios = require("axios");
 const qs = require("qs");
 
 // Salesforce OAuth 2.0 configuration
-const authConfig = {
-  instanceUrl: "https://test-18d-dev-ed.develop.my.salesforce.com", // Replace with your Salesforce instance URL
+const config = {
   authUrl: "https://login.salesforce.com/services/oauth2/authorize", // Use 'https://test.salesforce.com/services/oauth2/token' for sandbox
   clientId:
-    "3MVG9HB6vm3GZZR9BkkPcu_Raenzdn.wiKJGpBHKhpsjCMwhkYIAkmQ0du99AwQBf_8549KiqAVAww58V8TlN", // Replace with your Consumer Key
+    "3MVG9HB6vm3GZZR9BkkPcu_Raenzdn.wiKJGpBHKhpsjCMwhkYIAkmQ0du99AwQBf_8549KiqAVAww58V8TlN",
   clientSecret:
-    "7C928FF64B227019C9064200BC3E4F7FFA48E071929D14345D3D94622A9B0FD9", // Replace with your Consumer Secret
-  username: "sfdcad2023@gmail.com", // Replace with your Salesforce username
-  password: "Kolkata#1pYkMQp3YYLAaCGBlduTbekvy", // Replace with your Salesforce password and security token
+    "7C928FF64B227019C9064200BC3E4F7FFA48E071929D14345D3D94622A9B0FD9",
+  domain: "https://test-18d-dev-ed.develop.my.salesforce.com",
+  tokenUrl:
+    "https://test-18d-dev-ed.develop.my.salesforce.com/services/oauth2/token",
 };
 
 // Function to get Salesforce access token
 async function getAccessToken() {
-  const params = {
-    grant_type: "client_credentials",
-    response_type: "token",
-    // scope: "api",
-    redirect_uri: "https://oauth.pstmn.io/v1/browser-callback",
-    client_id: authConfig.clientId,
-    client_secret: authConfig.clientSecret,
-    // username: authConfig.username,
-    // password: authConfig.password,
-  };
-
   try {
     const response = await axios.post(
-      authConfig.authUrl,
-      qs.stringify(params),
+      config.tokenUrl,
+      new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: config.clientId,
+        client_secret: config.clientSecret,
+      }),
       {
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
       }
     );
+
     return response.data.access_token;
   } catch (error) {
     console.error(
@@ -48,13 +44,13 @@ async function getAccessToken() {
 async function createAccount(accessToken) {
   const accountData = {
     Name: "New Account", // Replace with the data you want to send
-    Phone: "123-456-7890",
-    Website: "http://www.example.com",
+    // Phone: "123-456-7890",
+    // Website: "http://www.example.com",
   };
 
   try {
     const response = await axios.post(
-      `${authConfig.instanceUrl}/services/data/v57.0/sobjects/Account/`,
+      `${config.domain}/services/data/v61.0/sobjects/Account/`,
       accountData,
       {
         headers: {
@@ -76,6 +72,8 @@ async function createAccount(accessToken) {
 async function main() {
   try {
     const accessToken = await getAccessToken();
+    console.log(accessToken);
+
     await createAccount(accessToken);
   } catch (error) {
     console.error("Error:", error.message);
